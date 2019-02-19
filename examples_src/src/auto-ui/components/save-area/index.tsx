cc460d8d687795ad33a52762124c53e97fee32e9
Eason chiu <76670758@qq.com>
2019-02-19 19:19:08 +0800

    fix(savearea): 修改安全区域在ios app中顶部计算错误

--- examples_src/src/auto-ui/components/save-area/index.tsx
+++ examples_src/src/auto-ui/components/save-area/index.tsx
@@ -9,10 +9,26 @@ interface IProps {
   children?: null;
 }
 
+const isiOS = () => {
+  return !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+};
+
+const isApp = () => {
+  return navigator.userAgent.indexOf('atzuche') > -1;
+};
+
 const SaveArea: React.FC<IProps> = props => {
   const composeClassName = cn(`x-save-area-inset-${props.inset}`, props.className);
-
-  return <div className={composeClassName} style={{ backgroundColor: props.color }} />;
+  const node = <div className={composeClassName} style={{ backgroundColor: props.color }} />;
+  if (props.inset === 'top' && isiOS && isApp) {
+    return (
+      <React.Fragment>
+        {node}
+        <div style={{ backgroundColor: props.color }} className="x-save-area-ios-app" />
+      </React.Fragment>
+    );
+  }
+  return node;
 };
 
 SaveArea.defaultProps = {
