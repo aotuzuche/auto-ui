c66484af2d0223ed467437fe05d7733b84069edd
Eason chiu <76670758@qq.com>
2019-02-19 16:56:29 +0800

    fix(index.d.ts): 遗漏navbar

--- index.d.ts
+++ index.d.ts
@@ -8,6 +8,7 @@ import Emitter from './es/emitter';
 import Input from './es/input';
 import Layout from './es/layout';
 import Loading from './es/loading';
+import NavBar from './es/nav-bar';
 import Popup from './es/popup';
 import Radio from './es/radio';
 import SaveArea from './es/savearea';
@@ -38,4 +39,5 @@ export {
   Tabs,
   Toast,
   TimePicker,
+  NavBar,
 };
