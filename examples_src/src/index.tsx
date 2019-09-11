b876acdd6db405ea507fdecccd3c7b24c6b68836
Eason chiu <76670758@qq.com>
2019-09-11 14:44:29 +0800

    update: gh-pages

--- examples_src/src/index.tsx
+++ examples_src/src/index.tsx
@@ -5,10 +5,6 @@ import './assets/style/reset.css';
 import './utils/flexible';
 import './utils/polyfill';
 
-// fastclick
-const FastClick = require('fastclick');
-FastClick.attach(document.body);
-
 const app = dva({ history: createHashHistory() });
 
 app.router(r => router(r!.history));
