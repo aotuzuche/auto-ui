366b4379417f7b9b060752b021dde4e6571768de
Eason chiu <76670758@qq.com>
2019-11-01 10:58:18 +0800

    fix(Card): 修复card的aspectRatio属性的错误

--- examples_src/src/auto-ui/components/card/index.tsx
+++ examples_src/src/auto-ui/components/card/index.tsx
@@ -12,11 +12,11 @@ interface IFigureProps {
 const CardFigure: React.FC<IFigureProps> = props => {
   const { image, title, children, onClick, ...otherProps } = props
 
-  const cls = cn('x-card-figure', { 'x-card-figure--activable': !!onClick })
+  const cls = cn('x-card__figure', { 'x-card__figure--activable': !!onClick })
 
   return (
     <figure {...otherProps} className={cls} onClick={onClick}>
-      <div className="x-card-figure-content">{children}</div>
+      <div className="x-card__figure-content">{children}</div>
       <img src={image} alt={title} title={title} />
     </figure>
   )
@@ -26,7 +26,7 @@ interface ICardProps {
   shadow?: boolean
   children: React.ReactNode
   direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse'
-  aspectRatio: number
+  aspectRatio?: number
   onClick?: () => void
   [otherProps: string]: any
 }
@@ -38,19 +38,16 @@ const Card: React.FC<ICardProps> & {
 
   const cls = cn('x-card', {
     'x-card--shadow': shadow,
+    'x-card--ar': aspectRatio && aspectRatio > 0,
     'x-card--activable': !!onClick,
   })
 
   return (
     <div {...otherProps} className={cls} onClick={onClick}>
-      <div className={`x-card-inner x-card--${direction || 'column'}`}>{children}</div>
-      <div
-        className="x-card-ar"
-        style={{
-          marginTop: `${(typeof aspectRatio === 'undefined' || aspectRatio < 0 ? 1 : aspectRatio) *
-            100}%`,
-        }}
-      />
+      <div className={`x-card__inner x-card--${direction || 'column'}`}>{children}</div>
+      {aspectRatio && aspectRatio > 0 ? (
+        <div className="x-card__ar" style={{ marginTop: `${aspectRatio * 100}%` }} />
+      ) : null}
     </div>
   )
 }
--- examples_src/src/pages/card.tsx
+++ examples_src/src/pages/card.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Card, Layout, Toast } from '../auto-ui'
+import { Card, Cell, Layout, Toast } from '../auto-ui'
 
 class Demo extends React.PureComponent {
   state = { disabled: false }
@@ -9,22 +9,67 @@ class Demo extends React.PureComponent {
   }
 
   render() {
-    const sty = {
-      height: '50%',
-    }
-
     return (
       <Layout>
         <Layout.Body>
-          <Card shadow aspectRatio={12 / 9} style={{ width: '50%' }}>
+          <Cell.Title text="不固定宽高比的card，高度随着内容的多少而变化" />
+          <Card shadow style={{ width: '50%' }}>
             <Card.Figure
-              style={sty}
+              style={{ height: '50%' }}
               onClick={this.onClick}
               image="https://carphoto.aotuzuche.com/car/19/07/687221426/1562663319417_1.jpg"
             >
-              <p style={{ color: 'white', padding: '10px' }}>text</p>
+              <p style={{ color: 'white', padding: '10px' }}>车辆名称</p>
             </Card.Figure>
-            <p style={{ padding: '10px' }}>hahahaha</p>
+            <p style={{ padding: '10px' }}>
+              车辆名称
+              <br />
+              品牌
+              <br />
+              车牌号
+              <br />
+              品牌
+              <br />
+              车牌号
+            </p>
+          </Card>
+
+          <Cell.Title text="固定宽高比的card，此处宽高比为1:1，图片占用50%高度，超出文字部分将隐藏" />
+          <Card shadow aspectRatio={1} style={{ width: '50%' }}>
+            <Card.Figure
+              style={{ height: '50%' }}
+              onClick={this.onClick}
+              image="https://carphoto.aotuzuche.com/car/19/07/687221426/1562663319417_1.jpg"
+            >
+              <p style={{ color: 'white', padding: '10px' }}>车辆名称</p>
+            </Card.Figure>
+            <p style={{ padding: '10px' }}>
+              车辆名称
+              <br />
+              品牌
+              <br />
+              车牌号
+              <br />
+              品牌
+              <br />
+              车牌号
+            </p>
+          </Card>
+
+          <Cell.Title text="左右排列，3:8比例，图片占50%宽度" />
+          <Card shadow aspectRatio={3 / 8} direction="row">
+            <Card.Figure
+              style={{ width: '50%' }}
+              onClick={this.onClick}
+              image="https://carphoto.aotuzuche.com/car/19/07/687221426/1562663319417_1.jpg"
+            >
+              <p style={{ color: 'white', padding: '10px' }}>车辆名称</p>
+            </Card.Figure>
+            <p style={{ padding: '10px' }}>
+              车辆名称
+              <br />
+              品牌
+            </p>
           </Card>
         </Layout.Body>
       </Layout>
