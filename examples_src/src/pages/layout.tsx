4fb9b7efd1b4bb2f6eeaa2b1d65f6415fea157a4
liuxingyun <liuxingyun2010@gmail.com>
2020-04-01 14:21:37 +0800

    test: 💍 layout
    layout demo for provider

--- examples_src/src/pages/layout.tsx
+++ examples_src/src/pages/layout.tsx
@@ -8,26 +8,8 @@ const Demo: React.FC<any> = () => {
         <Layout.Body>body</Layout.Body>
         <Layout.Footer>footer</Layout.Footer>
       </Layout>
-      {/* <A /> */}
     </LayoutProvider.Provider>
   )
-}
-
-const A = () => {
-  return (
-    <div>
-      dsadasdas
-      <B />
-    </div>
-  )
-}
-
-class B extends React.PureComponent {
-  static contextType = LayoutProvider
-  render() {
-    console.log(this.context)
-    return <div>123445</div>
-  }
 }
 
 export default Demo
