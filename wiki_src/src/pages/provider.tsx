877482b87b66b52308e0970505e1b81eeece826b
liuxingyun <liuxingyun2010@gmail.com>
2020-04-01 17:01:12 +0800

    fix: 🐛 Provider

---
+++ wiki_src/src/pages/provider.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import C from 'classnames';
+import MobilePreview from '../components/mobilePreview';
+
+export default class View extends React.PureComponent {
+  public state = {
+    previewUrl: 'https://aotuzuche.github.io/auto-ui/examples/#',
+  }
+
+  public render() {
+    return (
+      <div>
+        <div className="markdown-content">
+<h2>Provider</h2><p>用于服务端渲染时，把window等浏览器全局变量传给服务端</p>
+
+        </div>
+        <MobilePreview link={this.state.previewUrl} />
+      </div>
+    );
+  }
+
+  private onCodeClick = (url: string) => {
+    this.setState({
+      previewUrl: 'https://aotuzuche.github.io/auto-ui/examples/#' + url,
+    })
+  }
+}
+  
