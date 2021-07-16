0a0e1ab703e7f9e7fcb4e905c1b137eb3e6f7868
solin <chensl3316@outlook.com>
2021-07-16 17:48:23 +0800

    feat: radio 适配taro

--- examples_src/src/auto-ui/components/radio/index.tsx
+++ examples_src/src/auto-ui/components/radio/index.tsx
@@ -10,12 +10,16 @@ interface IProps {
   className?: string
   text?: string
   children?: null
+  /**
+   * @description 小程序中不支持传type 来改颜色，请直接传color
+   */
+  color?: string
   onChange?: (checked: boolean) => void
   [otherProps: string]: any
 }
 
 const Radio: React.FC<IProps> = props => {
-  const { checked, disabled, className, text, onChange, hollow, ...otherProps } = props
+  const { checked, disabled, className, text, onChange, hollow, color, ...otherProps } = props
 
   const composeClassName = cn(
     'x-radio',
@@ -35,21 +39,39 @@ const Radio: React.FC<IProps> = props => {
       onChange(!checked)
     }
   }
+  const bgColor = () => {
+    if(!hollow) {
+      return checked ? color : '#d3d3d3'
+    }
+    return '#ffffff'
+  }
+  const borderColor = () => {
+    if(!hollow) {
+      return undefined
+    }
+    return checked ? (color || '#00cf97') : '#d3d3d3'
+  }
+  const iconColor = () => {
+    if(!hollow) {
+      return '#ffffff'
+    }
+    return checked ? (color || '#00cf97') : '#d3d3d3'
+  }
   if (onChange) {
     return (
       <button {...otherProps} className={composeClassName} onClick={onClick}>
-        <i className="x-radio__icon">
-          <IconCheck />
-        </i>
+        <span className="x-radio__icon" style={{backgroundColor: bgColor(), boxShadow: `0 0 0 1rpx ${borderColor()} inset;`}}>
+          <IconCheck color={iconColor()}/>
+        </span>
         {!!text && <span className="x-radio__text">{text}</span>}
       </button>
     )
   }
   return (
     <span {...otherProps} className={composeClassName} onClick={onClick}>
-      <i className="x-radio__icon">
-        <IconCheck />
-      </i>
+        <span className="x-radio__icon" style={{backgroundColor: bgColor(), boxShadow: `0 0 0 1rpx ${borderColor()} inset;`}}>
+          <IconCheck color={iconColor()}/>
+      </span>
       {!!text && <span className="x-radio__text">{text}</span>}
     </span>
   )
